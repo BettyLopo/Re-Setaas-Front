cbf66742bb7e7c74c53a133c19e24db511867251
Betty Lopez <elisabetlopezpons@gmail.com>
2024-09-05 09:41:11 +0200

    Custom config to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,40 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      pink: "#F0A6CA",
+      darklila: "#362056",
+      lightlila: "#9C89B8",
+      softblue: "#B8BEDD",
+      brokenwhite: "#F0E6EF",
+      lightpink: "#EFC3E6",
+      brown: "#554348",
+    },
+    fontFamily: {
+      raleway: 'Raleway',
+      ultra: 'Ultra'
+    },
     extend: {
-      colors: {
-        pink: "#F0A6CA",
-        darklila: "#362056",
-        lightlila: "#9C89B8",
-        softblue: "#B8BEDD",
-        brokenwhite: "#F0E6EF",
-        lightpink: "#EFC3E6"
+      backgroundImage: {
+        'gr-pink-pink': 'linear-gradient(180deg, #EFC3E6 100%, #F0A6CA 100%)',
+        'gr-lila-pink': 'linear-gradient(180deg, #9C89B8 100%, #F0A6CA 100%)',  
+        'gr-blue-lila': 'linear-gradient(180deg, #B8BEDD 100%, #9C89B8 100%)',
+        'gr-white-blue': 'linear-gradient(180deg, #F0E6EF 100%, #B8BEDD 100%)'
       },
-      fontFamily: {
-        raleway: 'Raleway',
-        ultra: 'Ultra'
+      borderRadius: {
+        inp: '0.65rem',
+        cont: '1.25rem' 
+      },
+      fontSize: {
+        ptitle: '3.125rem',
+        stitle: '1.875rem',
+        ctitle: '1.25rem',
+        userp: '1.625rem',
+        regu: '1.5rem',
+        tiny: '1rem',
       }
     },
+    
   },
   plugins: [],
 }
