21e8ccdbc88c3f54cab0f2ab866f23851a069139
Betty Lopez <elisabetlopezpons@gmail.com>
2024-09-07 19:00:39 +0200

    Add page title, adjust margins and paddings of elements

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,14 +5,15 @@ import Button from '../components/buttons/Button'
 
 const Login = () => {
   return (
-    <section className="flex flex-col items-center justify-center bg-gr-pink-pink h-screen w-screen relative pb-[4rem] gap-[5rem]">
+    <section className="flex flex-col items-center justify-center bg-gr-pink-pink h-screen w-screen relative pb-[6rem] gap-[1.8rem]">
       <div className="fixed -z-9 inset-0">
           <img src="/public/img/seta_2.svg" alt="Seta decorativa" className="fixed -bottom-12 w-[80%] -right-4 -z-9 opacity-25"/>
           <img src="/public/img/seta_1.svg" alt="Seta decorativa" className="fixed -top-5 w-[30%] -left-5 -z-9 opacity-90 rotate-[145deg]"/>
         </div>
-      <h1 className="text-center font-ultra text-ptitle text-darklila leading-7 mb-7">Re<br/>(Setas)</h1>
-      <div className="flex flex-col relative gap-[3rem]">
-        <form action="" onSubmit="" className="flex flex-col justify-center gap-7">
+      <h1 className="text-center font-ultra text-ptitle text-darklila leading-7 my-[3.5rem]">Re<br/>(Setas)</h1>
+      <div className="flex flex-col relative gap-[2rem]">
+      <label htmlFor="login" className="text-center font-ultra text-darklila text-stitle ">Inicio sesión</label>
+        <form action="" onSubmit="" id="login" className="flex flex-col justify-center gap-7">
           <Input 
             placeholder="E-mail" 
             className="bg-brokenwhite w-[18.1rem]" 
@@ -30,11 +31,11 @@ const Login = () => {
             // value={password}
             onChange=""/>
         </form>
-        <div className="flex flex-row justify-center gap-4">
+        <div className="flex flex-row justify-center gap-7 mt-2">
           <Button className="bg-lightlila text-brokenwhite" type="button" onClick="" text="Aceptar"/>
           <Button className="bg-softblue  text-darklila" type="button" onClick="" text="Cancelar"/>
         </div>
-        <div className="text-center my-4">
+        <div className="text-center my-2">
           <p className="font-raleway text-regu font-semibold text-darklila">¿No tienes cuenta?</p>
           <p className="font-raleway text-regu font-semibold text-darklila">Registrate <Link to="/sign-in" className="font-ultra text-brokenwhite">aquí</Link></p>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,13 +6,14 @@ import InputImg from '../components/inputs/InputImg'
 
 const Register = () => {
   return (
-    <section className="flex flex-col items-center justify-center bg-gr-pink-pink h-screen w-screen relative pb-[4rem] gap-[5rem]">
+    <section className="flex flex-col items-center justify-center bg-gr-pink-pink h-screen w-screen relative pb-[6rem] gap-[1.8rem]">
       <div className="fixed -z-9 inset-0">
           <img src="/public/img/seta_1.svg" alt="Seta decorativa" className="fixed -bottom-10 w-[65%] -right-10 -z-9 opacity-60"/>
         </div>
       <h1 className="text-center font-ultra text-ptitle text-darklila leading-7 mb-7">Re<br/>(Setas)</h1>
-      <div className="flex flex-col relative gap-[3rem]">
-        <form action="" onSubmit="" className="flex flex-col justify-center gap-7">
+      <div className="flex flex-col relative gap-[2rem]">
+        <label htmlFor="registro" className="text-center font-ultra text-darklila text-stitle">Registro</label>
+        <form action="" onSubmit="" id="registro" className="flex flex-col justify-center gap-6">
         <Input 
             placeholder="Nombre" 
             className="bg-brokenwhite w-[18.1rem]" 
@@ -39,11 +40,11 @@ const Register = () => {
             onChange=""/>
             <InputImg />
         </form>
-        <div className="flex flex-row justify-center gap-4">
+        <div className="flex flex-row justify-center gap-7 mt-2">
           <Button className="bg-lightlila text-brokenwhite" type="button" onClick="" text="Registrar"/>
           <Button className="bg-softblue  text-darklila" type="button" onClick="" text="Cancelar"/>
         </div>
-        <div className="text-center my-4">
+        <div className="text-center my-2">
           <p className="font-raleway text-regu font-semibold text-darklila">¿Ya tienes cuenta?</p>
           <p className="font-raleway text-regu font-semibold text-darklila">Entra <Link to="/login" className="font-ultra text-brokenwhite">aquí</Link></p>
         </div>
